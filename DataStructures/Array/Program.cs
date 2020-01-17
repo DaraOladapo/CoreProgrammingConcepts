7b4feacb9ee7027b4c79bf20051afbaafe4cdaed
Dara Oladapo <dara.oladapo@outlook.com>
2020-01-17 15:22:24 +0000

    reorg projects into folders

---
+++ DataStructures/Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myStringArray=new string[]{
+                "Item1","Item2","Item3","Item4","Item5",
+                "Item6","Item7","Item8","Item9","Item10"
+            };
+            Console.WriteLine($"Item at index 2 is: {myStringArray[2]}" );
+            Console.WriteLine($"myStringArray contains {myStringArray.Length}");
+            Console.WriteLine("All the content of the myStringArray");
+            foreach (var arrayItem in myStringArray)
+            {
+                Console.WriteLine(arrayItem);
+            }
+        }
+    }
+}
---
+++ DataStructures/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Stack myStringStack=new Stack();
+           myStringStack.Push("Item1");
+           myStringStack.Push("Item2");
+           myStringStack.Push("Item3");
+           myStringStack.Push("Item4");
+           myStringStack.Push("Item5");
+           myStringStack.Push("Item6");
+           myStringStack.Push("Item7");
+           myStringStack.Push("Item8");
+           myStringStack.Push("Item9");
+           myStringStack.Push("Item10");
+           Console.WriteLine($"Initial stack count: {myStringStack.Count}");
+        }
+    }
+}
